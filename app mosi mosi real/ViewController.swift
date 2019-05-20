e7ccad354e328510971c48f14c9ce919e20a6542
jerry <minghojerry@icloud.com>
2019-05-20 15:23:14 +0700

    Initial Commit

---
+++ app mosi mosi real/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app mosi mosi real
+//
+//  Created by Jerry Ming Ho on 20/05/19.
+//  Copyright © 2019 Jerry Ming Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
